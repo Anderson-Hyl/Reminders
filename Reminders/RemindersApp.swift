b712a33e08ddfc2bc649e136769ae75cd5f49bc5
anderson <anderson@andersondeMacBook-Air.local>
2025-05-16 14:24:47 +0800

    project init

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by anderson on 2025/5/16.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RemindersTests/RemindersTests.swift
@@ -0,0 +1,16 @@
+//
+//  RemindersTests.swift
+//  RemindersTests
+//
+//  Created by anderson on 2025/5/16.
+//
+
+import Testing
+
+struct RemindersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
